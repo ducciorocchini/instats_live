2a9e9ff7d6c18f912fae81de52e19b4fa69ca0d7
Duccio Rocchini <ducciorocchini@gmail.com>
2025-01-16 16:06:03 +0100

    Create Session4_03_pca.R

---
+++ Code/Session4_03_pca.R
@@ -0,0 +1,18 @@
+# Multivariate analysis of remote sensing data
+
+library(imageRy)
+library(terra)
+
+im.list()
+
+sent <- im.import("sentinel.png")
+
+pairs(sent)
+
+sentpc <- im.pca(sent)
+pc1 <- sentpc[[1]]
+pc1sd <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
+
+plot(pc1sd)
+
+
